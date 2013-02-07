7b261bebedfd26e9e204481d1f5cfcdcfe136ae3
lcr <lcrees@gmail.com>
2013-02-07 11:50:46 -0700

    - process as Restructured Text
    --HG--
    rename : README.txt => README.rst

--- scripts/wikdShell.groovy
+++ scripts/wikdShell.groovy
@@ -3,9 +3,12 @@
 
 import groovy.ui.Console
 
+import org.freeplane.plugin.script.FreeplaneScriptBaseClass
+
 def binding = getBinding()
 
 def console = new Console(binding)
+console.setVariable('base', FreeplaneScriptBaseClass)
 console.setVariable('binding', binding)
 console.setVariable('map', node.map)
 console.setVariable('root', node.map.root)
